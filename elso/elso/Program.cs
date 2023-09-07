dd9406ed432a508ff92431f12b9de2426a2a848e
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2023-09-07 08:43:38 +0200

    a

--- elso/elso/Program.cs
+++ elso/elso/Program.cs
@@ -16,8 +16,52 @@ namespace elso
             Console.WriteLine(szam);
             string l = "Alma";
             Console.WriteLine(l);
-            char z = 'h';
+            char z = 'o';
             Console.WriteLine(z);
+            Console.WriteLine((char)(z+2));
+            
+
+            Console.WriteLine(10 / 3);
+            int i = 0;
+            ++i;
+            Console.WriteLine(i);
+            int a;
+            a = 5;
+            if(a>2)
+            {
+                Console.WriteLine("Nagyobb");
+            }
+
+            else if(a<2)
+            {
+                Console.WriteLine("Kisebb");
+            }
+            else if(a%2 ==0)
+            {
+                Console.WriteLine("páros");
+            }
+            else
+            {
+                Console.WriteLine("egyenlő");
+            }
+            switch(a)
+            {
+                case 1:
+                    Console.WriteLine("egy");
+                    break;
+                case 2:
+                    Console.WriteLine("keto");
+                    break;
+                case 3:
+                    Console.WriteLine("Háom");
+                    break;
+                case 4:
+                    Console.WriteLine("Négy");
+                    break;
+                default:
+                    Console.WriteLine("Valami más");
+                    break;
+            }
         }
     }
 }
