a699d72723f0db1d970525163ee0c78e1a8c0e47
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2023-09-08 12:30:39 +0200

    a

--- elso/elso/Program.cs
+++ elso/elso/Program.cs
@@ -18,8 +18,8 @@ namespace elso
             Console.WriteLine(l);
             char z = 'o';
             Console.WriteLine(z);
-            Console.WriteLine((char)(z+2));
-            
+            Console.WriteLine((char)(z + 2));
+
 
             Console.WriteLine(10 / 3);
             int i = 0;
@@ -27,16 +27,16 @@ namespace elso
             Console.WriteLine(i);
             int a;
             a = 5;
-            if(a>2)
+            if (a > 2)
             {
                 Console.WriteLine("Nagyobb");
             }
 
-            else if(a<2)
+            else if (a < 2)
             {
                 Console.WriteLine("Kisebb");
             }
-            else if(a%2 ==0)
+            else if (a % 2 == 0)
             {
                 Console.WriteLine("páros");
             }
@@ -44,7 +44,7 @@ namespace elso
             {
                 Console.WriteLine("egyenlő");
             }
-            switch(a)
+            switch (a)
             {
                 case 1:
                     Console.WriteLine("egy");
@@ -62,6 +62,67 @@ namespace elso
                     Console.WriteLine("Valami más");
                     break;
             }
+            double szam1 = 1;
+
+            while (szam1 < 5)
+            {
+                Console.WriteLine(szam1);
+                szam1 += 0.1;
+            }
+            do
+            {
+                szam1 -= 0.2;
+                Console.WriteLine(szam1);
+            }
+            while (szam1 > 2);
+
+            char betu = 'a';
+            
+            while(betu<='z')
+            {
+                Console.Write(betu);
+                betu = (char)(betu + 1);
+            }
+            Console.WriteLine();
+
+            int szam32 = 32;
+            while(szam32<=250)
+
+            {
+                Console.Write((char)szam32 + " ");
+                if (szam32 % 8 == 7)
+                {
+                    Console.WriteLine();
+                }
+                szam32++;
+
+            }
+            Console.WriteLine();
+
+
+            for (int k = 0; k <= 10; k++) 
+            {
+                Console.WriteLine(k);
+            }
+            int szamol = 0;
+            for(int g=1;g<=15;g++)
+            {
+                szamol += g;
+                
+            }
+            Console.WriteLine(szamol);
+
+            for (int d =9; d>0;d--)
+            {
+                Console.WriteLine(d)
+;           }
+            for(int c =0; c<10; c++)
+            {
+                Console.WriteLine(9 - c);
+            }
+
+
+
         }
     }
 }
