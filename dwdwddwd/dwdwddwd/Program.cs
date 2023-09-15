2b31a09a3148d11ebc032ef21501173314a6bf0a
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2023-09-15 11:29:58 +0200

    utolso nap amikor ebbe az iskolába látnak engem bucsuuzenet szeretm a sutit

---
+++ dwdwddwd/dwdwddwd/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+for (int k = 5; k <= 10; k++)
+{
+    Console.Write(k + " négyzete");
+
+    Console.Write(" "+ k * k + " ,");
+
+}
