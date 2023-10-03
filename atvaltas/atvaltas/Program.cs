3f40d4377b161a53e5d0a4b11cfb7cb4f89ad5e3
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2023-10-03 09:39:40 +0200

    a

---
+++ atvaltas/atvaltas/Program.cs
@@ -0,0 +1,27 @@
+﻿
+bool jo = false;
+while (!jo)
+{
+    jo = true;
+    Console.Write("Mit váltasak át? (pl: 12kg) :");
+    string be = Console.ReadLine();
+
+    string[] darab = be.Split(" ");
+    if(darab.Length!=2)
+    {
+        jo = false;
+        continue;
+    }
+    double szam = 0;
+    try
+    {
+        szam = Convert.ToDouble(darab[0]);
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Nem jó formátum! Legyen ilyen : 12,2kg");
+        jo = false;
+        continue;
+    }
+    Console.WriteLine(szam);
+}
---
+++ atvalto cucc mértékegység/atvalto cucc mértékegység/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Mit váltsak át? (pl:12kg) :");
+string be = Console.ReadLine();
+string[] darab = be.Split(' ');
+
+try
+{
+    double szam = Convert.ToDouble(darab[0]);
+}
+catch
+{
+    throw;
+}
+
+
+
