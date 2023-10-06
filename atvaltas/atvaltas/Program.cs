5a71e2d2111c75f0add467f5813b6975cff6deca
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2023-10-06 11:29:35 +0200

    a

--- atvaltas/atvaltas/Program.cs
+++ atvaltas/atvaltas/Program.cs
@@ -1,5 +1,20 @@
 ﻿
+
+string[] hosszusag = {"mm","cm","dm","m","km" };
+int[] hosszusagValto = { 10, 10, 10, 1000, 1 };
+string[] terulet = {"mm2","cm2","dm2","m2","km2"};
+int[] teruletValto = { 100, 100, 100, 1000000, 1 };
+string[] tomeg = { "g", "dkg", "kg", "t",};
+int[] tomegvalto = { 10, 100, 1000, 1 };
+
+
+
 bool jo = false;
+
+string forrasMertekEgyseg = "";
+double szam = 0;
+string[] egyseg = { };
+int[] valto = { };
 while (!jo)
 {
     jo = true;
@@ -7,12 +22,12 @@ while (!jo)
     string be = Console.ReadLine();
 
     string[] darab = be.Split(" ");
-    if(darab.Length!=2)
+    if (darab.Length != 2)
     {
         jo = false;
         continue;
     }
-    double szam = 0;
+    
     try
     {
         szam = Convert.ToDouble(darab[0]);
@@ -24,4 +39,61 @@ while (!jo)
         continue;
     }
     Console.WriteLine(szam);
+
+    if (hosszusag.Contains(darab[1]))
+    {
+        forrasMertekEgyseg = darab[1];
+        egyseg = hosszusag;
+        valto = hosszusagValto;
+    }
+    else if (terulet.Contains(darab[1]))
+    {
+        forrasMertekEgyseg = darab[1];
+        egyseg = terulet;
+        valto = teruletValto;
+    }
+    else
+    {
+        Console.WriteLine("nincs");
+        jo = false;
+        continue;
+    }
 }
+Console.WriteLine("Mire szeretnéd átváltani?");
+for (int i = 0; i < egyseg.Length; i++)
+{
+    Console.WriteLine(egyseg[i]);
+}
+string valasz = "";
+while(!egyseg.Contains(valasz))
+{
+    Console.Write("Válassz: ");
+    valasz = Console.ReadLine();
+}
+int index1=Array.IndexOf(egyseg, forrasMertekEgyseg);
+int index2=Array.IndexOf(egyseg, valasz);
+
+int szorzo = 1;
+//int[] hosszusagValto = {10,10,10,1000,1};
+
+
+double eredmeny = 0;
+if(index1<index2)
+{
+    for (int i = index1; i < index2; i++)
+    {
+        szorzo = szorzo * valto[i];
+    }
+    //Console.WriteLine(szorzo);
+    eredmeny = szam / szorzo;
+}
+else
+{
+    for (int i = index2; i < index1; i++)
+    {
+        szorzo = szorzo * valto[i];
+    }
+    //Console.WriteLine(szorzo);
+    eredmeny = szam * szorzo;
+}
+Console.WriteLine("{0} {1} = {2} {3}",szam,forrasMertekEgyseg,eredmeny,valasz);
