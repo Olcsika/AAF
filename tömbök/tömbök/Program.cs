529ac839413566f575006608495a12d7ad83d321
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2023-10-10 14:03:41 +0200

    a

---
+++ tömbök/tömbök/Program.cs
@@ -0,0 +1,27 @@
+﻿
+void kiir(int[]tomb)
+{
+    foreach (int i in tomb)
+    {
+        Console.WriteLine("{0}. szám", i);
+    }
+}
+
+int[] szamok = new int[100];   
+for (int i = 0; i < szamok.Length; i++)
+{
+    szamok[i] = i+1;
+}
+Console.WriteLine(szamok);
+foreach (int i in szamok)
+{
+    Console.WriteLine("{0}. szám",i);
+}
+for (int i = 0;i < szamok.Length;i++)
+{
+    szamok[i] = szamok[i]*2;
+    
+}
+kiir(szamok);
+
+//hazi feladat a szorzo tabla egyes tol 10 ig megvalositása 2 dimenzios tomben és kiirasa tablazat szeruen.
