5f4c6257284d504a4d524208636eb4e5decab0d4
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2023-11-17 08:44:42 +0100

    a

---
+++ listák gyakorlása mikor kijututtunk az EB re/listák gyakorlása mikor kijututtunk az EB re/Program.cs
@@ -0,0 +1,22 @@
+﻿
+List<int> szamok = new List<int>();
+
+Random rand = new Random();
+
+int darab=rand.Next(10000,100000);
+for (int i = 0; i < darab;  i++)
+{
+    szamok.Add(rand.Next(10,100));
+
+}
+
+Console.WriteLine("{0} darab szám van" ,szamok.Count);
+
+
+Dictionary<int, int> stat = new Dictionary<int, int>();
+
+for (int i = 0;i < szamok.Count;i++)
+{
+    //szamok[i] az aktuális elem
+
+}
